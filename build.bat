8dc5ed17afe9d3e51244a9c7b23b73ded965da2a
Amiagin <nigaima@gmail.com>
2013-01-04 14:47:44 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -9,18 +9,6 @@ rem Developer:  Cris del Rosario
 rem Git         https://github.com/Amiagin/antifs.git
 rem -------------------------------------------------------------------------
 
-pushd
-
-javac -classpath .;%ANT_HOME%\lib\ant.jar com\wolf\ant\*.java
-mkdir bin\com\wolf\ant
-move /Y com\wolf\ant\*.class bin\com\wolf\ant
+javac -classpath .;%ANT_HOME%\lib\ant.jar src\com\wolf\ant\*.java -d bin
 copy /Y antifs.properties bin
-cd bin
-
-del /Q  *.jar
-jar cvf antifs.jar .
-del antifs.properties
-rmdir /S/Q  com
-cd ..
-
-popd
+jar -cvf antifs.jar -C bin\ .
